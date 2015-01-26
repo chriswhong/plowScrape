af944b86944123d2ed546aad3675f193d6f6d60a
Chris Whong <chris.whong@socrata.com>
2015-01-26 10:11:15 -0500

    Removed Express Stuff

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 
 
+process.chdir(__dirname);
 
 var request = require('request')
-	fs = require('fs'),
-	express = require('express');
-
-var app = express();
-app.use(express.static(__dirname + '/public'));
+	fs = require('fs');
 
 var snowPlowEndpoint = 'http://maps.nyc.gov/geoserver/wfs?request=GetFeature&version=1.1.1&typeName=public:SNW_PLOWED&outputFormat=application/json'
 
 request(snowPlowEndpoint, function (error, response, body) {
   if (!error && response.statusCode == 200) {
   	var timestamp = new Date().getTime().toString();
-    fs.writeFile('public/data/' + timestamp + '.json', JSON.stringify(body), function(err) {
+    fs.writeFile('data/' + timestamp + '.json', JSON.stringify(body), function(err) {
     	console.log(err);
   	});
   }
